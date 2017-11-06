47a376047b3ed23b2c0cdc131cfeb5f589bdd732
DarkMarmot <scott.southworth@gmail.com>
2017-11-06 11:03:23 -0600

    woops forgot the js files for the demo

--- public/app/lovelace.js
+++ public/app/lovelace.js
@@ -8,13 +8,11 @@ Machine.cog({
     display:
 
         '<div style="padding: 10px; font-size: 12px; opacity: .5;">' +
-            'Ada has arrived...' +
+            'Ada has arrived... <span style="font-size: 24px; opacity: 0;">It\'s alright we told you what to dream.</span>' +
         '</div>',
 
     libs: {
         d3: 'D3'
-    },
-
-
+    }
 
 });
